b08c7dff4df136756952818f5e80d33a741e8538
Binlet Binu <binletbinu@gmail.com>
2023-10-07 08:56:29 +0530

    Added the NavBar of Airbnb

---
+++ public/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Navbar() {
+    return (
+        <nav className='nav'>
+            <img className='navbar-logo' src="../src/assets/images/airbnb-logo.png" />
+        </nav>
+    );
+}
+
+export default Navbar;
